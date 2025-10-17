0b9bd35962be830c91016807aeaa25cbf717e222
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-10-17 15:32:26 +0200

    problema 1 del examen 2024

---
+++ examen1/problema1.js
@@ -0,0 +1,20 @@
+let numero = parseInt(prompt("Dóna'm un número enter"));
+
+// console.log(numero);
+
+if (numero > 0) {
+    
+    //Acció si la condició es true
+    console.log("El número és positiu");
+
+} else if (numero < 0) {
+    
+    //Acció si la condició else if es true
+    console.log("El número és negatiu");
+
+} else {
+
+    //Acció si la condició es false
+    console.log("El número és zero");
+
+}
