8cda51c7c16819a4d83896b766ae3b3c9062bc16
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-10-17 09:10:30 +0200

    Update exemples-dowhile.js

--- JS/exemples-dowhile.js
+++ JS/exemples-dowhile.js
@@ -1,22 +1,22 @@
-let contador = 5;
+let contador = 1;
 do {      
     console.log("El valor de contador és: " + contador);
     contador++;
-} while (contador >= 5);
+} while (contador <= 5);
 
 // 🔁 Exemple: Simular la lectura del nivell de contaminació d’un sensor
 
 let nivellContaminacio;
 
 // Simulem la lectura d’un sensor (valor entre 0 i 100)
-// do {
-//   nivellContaminacio = Math.floor(Math.random() * 101);
-//   console.log("Nivell de contaminació llegit: " + nivellContaminacio);
-// } while (false); // Ens assegurem que s’executa un cop
+do {
+  nivellContaminacio = Math.floor(Math.random() * 101);
+  console.log("Nivell de contaminació llegit: " + nivellContaminacio);
+} while (false); // Ens assegurem que s’executa un cop
 
-// // Avaluem el resultat
-// if (nivellContaminacio > 50) {
-//   console.log("⚠️ Alerta: nivell alt de contaminació!");
-// } else {
-//   console.log("✅ Nivell segur de contaminació.");
-// }
+// Avaluem el resultat
+if (nivellContaminacio > 50) {
+  console.log("⚠️ Alerta: nivell alt de contaminació!");
+} else {
+  console.log("✅ Nivell segur de contaminació.");
+}
