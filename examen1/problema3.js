fae4ff90ea9a494f55bfec663f29f87dab36e9b8
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-10-17 16:21:17 +0200

    Update problema3.js

--- examen1/problema3.js
+++ examen1/problema3.js
@@ -1,5 +1,6 @@
 // 3.- (2 punts) Escriu un programa que demani a l'usuari un número N i mostri a la consola tots els números de l'1 fins a N. Pots fer-ho utilitzant tant un bucle for com un bucle while.
 
+//Tenint en compte que N és un número enter positiu
 let N = parseInt(prompt("Introdueix un número N:"));
 let i =1; // Variable per al bucle
 while (N >= 1) {
