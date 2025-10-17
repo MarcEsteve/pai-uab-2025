511d95422f14cc2c8b5c1ddef22c4bae07643ad0
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-10-17 17:38:05 +0200

    Update problema1.js

--- examen1/problema1.js
+++ examen1/problema1.js
@@ -2,13 +2,13 @@ let numero = parseInt(prompt("Dóna'm un número enter"));
 
 // console.log(numero);
 
-if (numero > 0) {
+if (numero === 0) {
     //Acció si la condició es true
-    console.log("El número és positiu");
-} else if (numero < 0) {
+    console.log("El número és zero");
+} else if (numero > 0) {
     //Acció si la condició else if es true
-    console.log("El número és negatiu");
+    console.log("El número és positiu");
 } else {
     //Acció si la condició es diferent de les anteriors
-    console.log("El número és zero");
+    console.log("El número és negatiu");
 }
