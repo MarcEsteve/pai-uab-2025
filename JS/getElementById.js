d663da6e5b37fa374e0837670658b88654c28fdd
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-11-20 18:12:59 +0100

    Add DOM manipulation examples and scripts
    Added an HTML file with various elements for DOM manipulation practice. Included JavaScript files demonstrating usage of getElementById and getElementsByTagName to modify element styles.

---
+++ JS/getElementById.js
@@ -0,0 +1,5 @@
+// Selecciona el elemento con ID 'uniqueElement'
+let uniqueElement = document.getElementById("uniqueElement");
+//A quién?¿ al div REF.
+uniqueElement.style.backgroundColor = "yellow";
+//Acción al div con id='uniqueElement'
---
+++ JS/getElementsByTagName.js
@@ -0,0 +1,13 @@
+// Selecciona todos los elementos <h2>
+let subtitles = document.getElementsByTagName("h2");
+for (let i = 0; i < subtitles.length-1; i++) {
+  subtitles[i].style.color = "blue";
+}
+
+// console.log(subtitles.length);
+
+// subtitles[0].style.color = "blue";
+
+// subtitles[1].style.color = "red";
+
+// subtitles[2].style.color = "green";
