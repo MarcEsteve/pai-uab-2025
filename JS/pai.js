8100ba90718a0b55701dfbc4d8fdb7b2185ea8b1
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-11-21 16:45:08 +0100

    exemples propietats DOM

--- JS/pai.js
+++ JS/pai.js
@@ -2,17 +2,14 @@
 console.log("Nombre d'enllaços:", document.links.length);
 console.log("Primer enllaç:", document.links[0].href);
 
-// 2. anchors
-console.log("Nombre d'anchors:", document.anchors.length);
-
-// 3. length (exemple amb imatges)
+// 2. length (exemple amb imatges)
 console.log("Nombre d'imatges:", document.images.length);
 
-// 4. innerHTML (modificar contingut)
+// 3. innerHTML (modificar contingut)
 document.getElementById("info").innerHTML =
   "Benvinguts a PAI! Aquí aprendreu a interactuar amb el DOM.";
 
-// 5. value + 6. forms
+// 4. value + 5. forms
 function llegirValor() {
   let formulari = document.forms[0];
   let valor = document.getElementById("inputSensor").value;
@@ -21,11 +18,11 @@ function llegirValor() {
     "Has introduït un valor de PM2.5: " + valor + " µg/m³";
 }
 
-// 7. images (modificar imatge)
+// 6. images (modificar imatge)
 setTimeout(() => {
-  document.images[0].src = "ciutat2.jpg";
+  document.images[0].src = "../IMG/ciutat2.jpg";
   console.log("Imatge canviada després de 2 segons");
 }, 2000);
 
-// 8. scripts
+// 7. scripts
 console.log("Scripts carregats:", document.scripts.length);
