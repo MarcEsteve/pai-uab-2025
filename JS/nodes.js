0bf9f7ac25badb32334ab990c742278dc46c8754
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-11-21 16:39:52 +0100

    Exemple creació elements i nodes DOM

---
+++ JS/nodes.js
@@ -0,0 +1,41 @@
+// Funció per afegir un sensor normal (appendChild + createElement + createTextNode)
+function afegirSensor() {
+  let llista = document.getElementById("llistaSensors");
+
+  // Crear l'element <li>
+  let nouSensor = document.createElement("li");
+
+  // Crear el text del sensor
+  let text = document.createTextNode("Sensor nou: Humitat — 48%");
+
+  // Afegir el text dins del <li>
+  nouSensor.appendChild(text);
+
+  // Afegir el <li> al final de la llista
+  llista.appendChild(nouSensor);
+}
+
+// Funció per afegir un sensor prioritari (insertBefore)
+function afegirSensorPrioritari() {
+  let llista = document.getElementById("llistaSensors");
+
+  // Crear element
+  let sensorUrgent = document.createElement("li");
+  let text = document.createTextNode("⚠ Sensor prioritari: Soroll — 78 dB");
+
+  sensorUrgent.appendChild(text);
+
+  // Inserir-lo abans del primer element existent
+  let primer = llista.firstElementChild;
+  llista.insertBefore(sensorUrgent, primer);
+}
+
+// Funció per eliminar l'últim sensor (removeChild)
+function eliminarUltimSensor() {
+  let llista = document.getElementById("llistaSensors");
+  let ultim = llista.lastElementChild;
+
+  if (ultim) {
+    llista.removeChild(ultim);
+  }
+}
